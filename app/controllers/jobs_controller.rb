3a571c54464222ac5dbf476993de76660fec51cc
Leight Johnson <leightjohnson93@gmail.com>
2018-06-14 15:57:47 -0400

    fixed the update/new issue

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -2,20 +2,16 @@ class JobsController < ApplicationController
   def index
     @jobs = Job.all
   end
-
   def show
     @job = Job.find(params[:id])
   end
-
   def new
     @job = Job.new
   end
 
   def edit
     @job = Job.find(params[:id])
-    #render "edit"
   end
-
   def create
     @job = Job.new(job_params)
     if @job.save
@@ -26,10 +22,8 @@ class JobsController < ApplicationController
       render 'new'
     end
   end
-
   def update
     @job = Job.find(params[:id])
-
     if @job.update(job_params)
       flash[:success] = 'Job was successfully updated.'
       redirect_to @job
@@ -38,15 +32,13 @@ class JobsController < ApplicationController
       render 'edit'
     end
   end
-
   def destroy
     @job = Job.find(params[:id])
     @job.destroy
     redirect_to jobs_path
   end
-
-private
-  def job_params
-    params.permit(:wv, :part, :line, :coat_date, :coat_time, :description, :lot_quantity, :test_quantity, :bin, :mfg_int)
-  end
+  private
+    def job_params
+      params.require(:job).permit(:wv, :line, :part, :coat_date, :coat_time, :description, :lot_quantity, :test_quantity, :bin, :mfg_int)
+    end
 end
