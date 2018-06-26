d54333ff1aa6e614d89e07e9c1fef4c4414a54c1
Leight Johnson <leightjohnson93@gmail.com>
2018-06-26 16:14:08 -0400

    Fixed validation for 'new'

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,6 +1,4 @@
 class Job < ApplicationRecord
-  belongs_to :part,#, optional: true,
-    :foreign_key => 'part_id'
-
-  validates :wv, :part_id, :coat_date, :coat_time, :lot_quantity, :test_quantity, :mfg_int, presence: true
+  belongs_to :part
+  validates :wv, :coat_date, :coat_time, :lot_quantity, :test_quantity, :mfg_int, presence: true
 end
