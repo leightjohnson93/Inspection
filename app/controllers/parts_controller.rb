9a8e4fb2bf08b2adbc83f3907718b8ee91ea007d
Leight Johnson <leightjohnson93@gmail.com>
2018-06-29 15:36:17 -0400

    created parts index, sortable table

--- app/controllers/parts_controller.rb
+++ app/controllers/parts_controller.rb
@@ -1,11 +1,25 @@
 class PartsController < ApplicationController
+  helper_method :sort_column, :sort_direction
+
+  def index
+    @parts = Part.order("#{sort_column} #{sort_direction}")
+  end
   def create
-    @job = Job.find(params[:job_id])
-    @part = @job.parts.find(part_params)
-    redirect_to job_path(@job)
   end
 
   private
+    def sortable_columns
+      ['part', 'description', 'shape', 'color', 'coating', 'mfg']
+    end
+
+    def sort_column
+      sortable_columns.include?(params[:column]) ? params[:column] : 'part'
+    end
+
+    def sort_direction
+      %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
+    end
+
     def part_params
       params.require(:part).permit(:part, :description, :color, :coating, :shape, :mfg)
     end
