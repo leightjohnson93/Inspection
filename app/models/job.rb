339975a3d90e8d00b20fa29e3e360dcfd1338e5b
Leight Johnson <leightjohnson93@gmail.com>
2018-08-01 11:38:48 -0400

    id unique validation

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,5 +1,6 @@
 class Job < ApplicationRecord
   belongs_to :part
   validates :coat_time, :lot_quantity, :test_quantity, presence: true
+  validates :id, uniqueness: true
   validates :lot_quantity, numericality: { less_than_or_equal_to: 9000}
 end
