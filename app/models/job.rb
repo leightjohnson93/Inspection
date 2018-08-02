2829a71fb2642b69cd90220a27c4a5547c6dbf36
Leight Johnson <leightjohnson93@gmail.com>
2018-08-02 08:15:22 -0400

    remove uniqueness

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,6 +1,5 @@
 class Job < ApplicationRecord
   belongs_to :part
-  validates :coat_time, :lot_quantity, :test_quantity, presence: true
-  validates :id, uniqueness: true
+  validates :id, :coat_time, :lot_quantity, :test_quantity, presence: true
   validates :lot_quantity, numericality: { less_than_or_equal_to: 9000}
 end
