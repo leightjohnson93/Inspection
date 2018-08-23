76baf4aa1cbc225659265b5ae4c19a2ede5a3284
Leight Johnson <leightjohnson93@gmail.com>
2018-08-22 20:45:35 -0400

    another migration fix

--- db/migrate/20180822231958_ballistic_fields.rb
+++ db/migrate/20180822231958_ballistic_fields.rb
@@ -10,11 +10,11 @@ class BallisticFields < ActiveRecord::Migration[5.2]
     add_column :jobs, :ballistic_spalls_4, :integer
     rename_column :jobs, :ballistic_assess, :ballistic_assess_1
     add_column :jobs, :ballistic_assess_2, :boolean
-    add_column :jobs, :ballistic_assess_3, :bolean
-    add_column :jobs, :ballistic_assess_4, :bolean
+    add_column :jobs, :ballistic_assess_3, :boolean
+    add_column :jobs, :ballistic_assess_4, :boolean
 
     (1..4).each do |n|
-      add_column :jobs, "ballistic_penetrations_#{n}", :integers
+      add_column :jobs, "ballistic_penetrations_#{n}", :integer
       add_column :jobs, "ballistic_results_#{n}", :float
     end
   end
