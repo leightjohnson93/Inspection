d4ae5a3c35c677c094db13263a8b0d513675bc25
Leight Johnson <leightjohnson93@gmail.com>
2018-08-22 20:36:33 -0400

    Modify ballistic fields

---
+++ db/migrate/20180822231958_ballistic_fields.rb
@@ -0,0 +1,21 @@
+class BallisticFields < ActiveRecord::Migration[5.2]
+  def change
+    add_column :jobs, :ballistic_method_3, :string
+    add_column :jobs, :ballistic_method_4, :string
+    add_column :jobs, :ballistic_samples_3, :integer
+    add_column :jobs, :ballistic_samples_4, :integer
+    add_column :jobs, :ballistic_fractures_3, :integer
+    add_column :jobs, :ballistic_fractures_4, :integer
+    add_column :jobs, :ballistic_spalls_3, :integer
+    add_column :jobs, :ballistic_spalls_4, :integer
+    rename_column :jobs, :ballistic_assess, :ballistic_assess_1
+    add_column :jobs, :ballistic_assess_2, :boolean
+    add_column :jobs, :ballistic_assess_3, :bolean
+    add_column :jobs, :ballistic_assess_4, :bolean
+
+    (1..4).each do |n|
+      add_column :jobs, "ballistic_penetrations_#{n}", :integers
+      add_column :jobs, "ballistic_results_#{n}", :float
+    enda
+  end
+end
