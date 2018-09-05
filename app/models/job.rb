f4b67e94136e95f4e1541985d3113b8598c69338
Leight Johnson <leightjohnson93@gmail.com>
2018-09-05 07:26:01 -0400

    fixed upcase for new lots

--- app/models/job.rb
+++ app/models/job.rb
@@ -4,5 +4,5 @@ class Job < ApplicationRecord
   validates :lot_quantity, numericality: { less_than_or_equal_to: 9000 }
   validates :id, :uniqueness =>  {:message => "has already been created." }
   validates :id, format: {without: /WV/, message: "cannot contain 'WV'."}
-  before_save { |job| job.qc_int.upcase! }
+  before_save { |job| job.qc_int.upcase! if qc_int}
 end
